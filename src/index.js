def595042aec5303d043f015367f7469d60a3f35
CJPohl <cjpohldev@gmail.com>
2021-10-11 16:05:27 -0400

    Added open weather API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+// TODO:
+// - Pick out important info from API
+// - Create object/class for weather statistics
+// - Stylize weather elements and find corresponding icons
+// - Add error control
+// - Maybe research eslint issue
+
 import './style.css';
 import { staticEls } from './elements';
 
--- src/script.js
+++ src/script.js
@@ -1,6 +1,10 @@
 import { staticEls } from "./elements";
 
 const scripts = (() => {
+    // set temp type
+    const DEFAULT_TEMP_MODE = 'f';
+    let tempMode = DEFAULT_TEMP_MODE;
+    
     // fetch giphy api
     async function getGifBackground() {
         const gif = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=xZt9fYZwOC1114gvJ6EiMRefocxMPKTs&s=weather`, {mode: 'cors'});
@@ -17,9 +21,18 @@ const scripts = (() => {
         if ((e.keyCode === 13) && (location.value !== '')) {
             staticEls.renderPage();
             staticEls.createContainers();
+            _searchWeather(location.value);
         }
         });
     };
+
+
+    // use the api to return important weather details
+    async function _searchWeather(location) {
+        const weather = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=a4ee5fcc3386c99af852f2e5c67c9976`)
+        const weatherJSON = await weather.json();
+        console.log(weatherJSON);
+    }
     
 
     return {
